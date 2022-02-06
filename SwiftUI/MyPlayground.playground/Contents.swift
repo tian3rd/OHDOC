d78f03cce1b9f65b053fdd170d74b57698a7c1c6
tian <tianwu@outlook.com>
2022-02-06 23:18:04 +1100

    add array, dict, enum

--- SwiftUI/MyPlayground.playground/Contents.swift
+++ SwiftUI/MyPlayground.playground/Contents.swift
@@ -15,3 +15,36 @@ print("Is game over?\n\(isGameOver)")
 var temperature = Double(20)
 var temperatureFahrenheit = temperature * 1.8 + 32
 print("Celsius: \(temperature)°C\nFahrenheit: \(temperatureFahrenheit)°F")
+
+// initialize a new array
+var numArr = Array<Int>()
+numArr.append(0)
+numArr.append(1)
+print(numArr.count)
+
+// initialize a new dictionary
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Kobe"] = 198
+print(heights)
+
+let olympics = [2008: "Beijing", 2012: "London"]
+print(olympics[2008, default: "BJ"])
+print(olympics.keys.count)
+print(olympics.values)
+
+// initialize a new set
+var actors = Set<String>()
+actors.insert("Tom Hanks")
+actors.insert("Jude Law")
+actors.insert("Danziel Washington")
+print(actors.sorted())
+
+// initialize an enum type
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day = Weekday.monday
+day = .tuesday
+print(day)
