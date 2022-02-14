3539e6e633b72c7e691e297668abe7cd76163560
tian <tianwu@outlook.com>
2022-02-14 20:23:41 +1100

    add struct & checkpoint 6

--- SwiftUI/MyPlayground.playground/Contents.swift
+++ SwiftUI/MyPlayground.playground/Contents.swift
@@ -303,3 +303,64 @@ let luckierNumbers = luckyNumbers.filter {
     return luckyString
 }
 print(luckierNumbers)
+
+// struct
+
+//You can create your own structs by writing struct, giving it a name, then placing the struct’s code inside braces.
+//Structs can have variable and constants (known as properties) and functions (known as methods)
+//If a method tries to modify properties of its struct, you must mark it as mutating.
+//You can store properties in memory, or create computed properties that calculate a value every time they are accessed.
+//We can attach didSet and willSet property observers to properties inside a struct, which is helpful when we need to be sure that some code is always executed when the property changes.
+//Initializers are a bit like specialized functions, and Swift generates one for all structs using their property names.
+//You can create your own custom initializers if you want, but you must always make sure all properties in your struct have a value by the time the initializer finishes, and before you call any other methods.
+//We can use access to mark any properties and methods as being available or unavailable externally, as needed.
+//It’s possible to attach a property or methods directly to a struct, so you can use them without creating an instance of the struct.
+
+// checkpoint 6
+struct Car {
+    private var model: String
+    private var seats: Int
+    
+    init(model: String, seats: Int) {
+        self.model = model
+        self.seats = seats
+    }
+    
+    var currentGear: Int = 0 {
+        didSet {
+            print("Current gear is \(currentGear)")
+            print("Old gear is \(oldValue)")
+        }
+    }
+    
+    var modelInfo: String {
+        get {
+            return model
+        }
+    }
+    
+    var seatsInfo: Int {
+        get {
+            return seats
+        }
+    }
+    
+    mutating func changeGear(change: Int) {
+        currentGear += change
+        if currentGear > 10 || currentGear < 0 {
+            print("Illegal gear setup, abort...")
+            return
+        }
+        if change > 0 {
+            print("Changing gear up")
+        } else {
+            print("Changing gear down")
+        }
+    }
+}
+
+var myCar = Car(model: "Koleos", seats: 5)
+print("My car is \(myCar.modelInfo) and have \(myCar.seatsInfo) seats")
+myCar.changeGear(change: 10)
+myCar.changeGear(change: 1)
+
